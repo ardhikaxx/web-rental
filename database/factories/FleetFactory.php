8587b8888a7fc0906665234ec0508129106c64ff
ardhikaxx <ardhikayanuar58@gmail.com>
2026-08-07 06:27:28 +0700

    feat: factory armada

---
+++ database/factories/FleetFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Fleet;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Fleet>
+ */
+class FleetFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
