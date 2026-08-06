552418b51944d17cb40272c1c7ccc227e2a4e42b
ardhikaxx <ardhikayanuar58@gmail.com>
2026-08-07 06:27:50 +0700

    feat: kontroler kategori armada

---
+++ app/Http/Controllers/FleetCategoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FleetCategory;
+use Illuminate\Http\Request;
+
+class FleetCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FleetCategory $fleetCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FleetCategory $fleetCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, FleetCategory $fleetCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FleetCategory $fleetCategory)
+    {
+        //
+    }
+}
