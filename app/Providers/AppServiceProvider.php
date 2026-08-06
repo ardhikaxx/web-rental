2b9a95b11d40c2416243eeaa2df360e3bc41a91a
ardhikaxx <ardhikayanuar58@gmail.com>
2026-08-07 06:27:48 +0700

    feat: registrasi format tanggal & helper

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,31 @@
 
 namespace App\Providers;
 
+use App\Models\SiteSetting;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        $settings = SiteSetting::allAsArray();
+        // always referenced views
+        View::share('site', $settings);
+        View::share('setting', SiteSetting::class);
+
+        Blade::directive('rupiah', function ($expression) {
+            return "<?php echo 'Rp ' . number_format((float) $expression, 0, ',', '.'); ?>";
+        });
+
+        Blade::directive('dateid', function ($expression) {
+            return "<?php echo ($expression) ? \\Carbon\\Carbon::parse($expression)->translatedFormat('d F Y') : '-'; ?>";
+        });
     }
 }
