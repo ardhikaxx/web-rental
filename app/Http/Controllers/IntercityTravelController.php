a705dcab6d57201d3cde538bd91d66b985d3fce7
ardhikaxx <ardhikayanuar58@gmail.com>
2026-08-07 06:27:50 +0700

    feat: kontroler travel antar kota

---
+++ app/Http/Controllers/IntercityTravelController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\IntercityTravel;
+use Illuminate\Http\Request;
+
+class IntercityTravelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(IntercityTravel $intercityTravel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(IntercityTravel $intercityTravel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, IntercityTravel $intercityTravel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(IntercityTravel $intercityTravel)
+    {
+        //
+    }
+}
